9776722c241aa4a78b8a874731ec690b95087a96
Ankit Bansal <ankit.bansal.id@gmail.com>
2018-08-11 20:44:39 +0530

    React Form validate | adds basic setup

---
+++ build/elements/Constants.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var REGEXP_PREFIX = exports.REGEXP_PREFIX = "regex_";
+var ERROR_MSGS = exports.ERROR_MSGS = {
+  DEFAULT: 'There was an error',
+  FIELD_EMPTY: 'This field cannot be empty',
+  PWD_INVALID: 'Password should contain at least one uppercase alphabet, digit and a special character (!, @, $, %)',
+  FAILED: 'failed',
+  INVALID_EMAIL: 'Please enter a valid email ID',
+  INVALID_CIVILID: 'Invalid Civil Id format',
+  SP_CH_NOT_ALLOWED: 'Special character not allowed',
+  NO_SPACES: 'No spaces allowed',
+  THESE_SP_CH_NOT_ALLOWED: 'This special character is not allowed. Characters allowed are  ! @ $ and %',
+  NO_NUMBERS: 'Numbers are not allowed',
+  ONLY_ALPHABETS: 'Only Alphabets are allowed',
+  INVALID_INPUT: 'Please enter valid input'
+};
